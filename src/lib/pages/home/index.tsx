79ec5d005d21b590a69ae68686a12b31d7b7be8a
Marilok <42029758+Marilok@users.noreply.github.com>
2024-01-10 22:22:53 +0100

    init

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,4 +1,5 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
+import { IconBrandGithub, IconBrandInstagram, IconBrandLinkedin } from '@tabler/icons-react';
 
 import CTASection from '~/lib/components/samples/CTASection';
 import SomeImage from '~/lib/components/samples/SomeImage';
@@ -15,6 +16,25 @@ const Home = () => {
       mb={8}
       w="full"
     >
+      <p>Inspiration: https://v4.brittanychiang.com/</p>
+      <h1>Hi, I'm Marek</h1>
+      <h2>Some Things I've Built</h2>
+      <p>Some title</p>
+      <p>Some description</p>
+      <p>Some image</p>
+      <p>Github + URL links</p>
+      <h2>Let's connect</h2>
+      <p>Github</p>
+      <p>LinkedIn</p>
+      <p>Email</p>
+      <Button colorScheme="linkedin" leftIcon={<IconBrandLinkedin />}>
+        LinkedIn
+      </Button>
+      <Button colorScheme="black" leftIcon={<IconBrandGithub />}>
+        GitHub
+      </Button>
+
+      <p>I'm always open to hear new opportunities and ideas.</p>
       <SomeText />
       <SomeImage />
       <CTASection />
